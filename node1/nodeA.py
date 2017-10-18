fd17d74abe234363113bfba0b54f525f4282caa2
HUANG Manutea <manutea.huang@insa-rennes.fr>
2017-10-18 09:08:50 +0200

    nodeB try to join a lorawan

--- node1/nodeA.py
+++ node1/nodeA.py
@@ -2,13 +2,15 @@ from network import LoRa
 import socket
 import time
 
-lora = LoRa(mode=LoRa.LORA, frequency=863000000)
+lora = LoRa(mode=LoRa.LORA, frequency=868100000)
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-s.setblocking(False)
+s.setblocking(True)
 
 while True:
     print('ok')
-    if s.recv(64) == b'Ping':
-        s.send('Pong')
-        print('receive ping -> send pong')
-    time.sleep(5)
+    s.send('coucou')
+    #print(s.recv(64))
+    #if s.recv(64) == b'Ping':
+    #    s.send('Pong')
+    #    print('receive ping -> send pong')
+    time.sleep(0.1)
